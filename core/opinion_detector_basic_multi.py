233bccab6b2f3db1b297e736265ba1b577d9b3af
Giannis Melidis <giannismelidis@olery.com>
2014-05-01 11:56:24 +0200

    removed lxml from pre_install.

--- core/opinion_detector_basic_multi.py
+++ core/opinion_detector_basic_multi.py
@@ -9,13 +9,12 @@ this_folder = os.path.dirname(os.path.realpath(__file__))
 # This updates the load path to ensure that the local site-packages directory
 # can be used to load packages (e.g. a locally installed copy of lxml).
 sys.path.append(os.path.join(this_folder, 'site-packages/pre_build'))
-sys.path.append(os.path.join(this_folder, 'site-packages/pre_install'))
 
-from lxml import etree
 from VUKafParserPy import KafParser
 from collections import defaultdict
 import operator
 import pprint
+import lxml
 from lxml import etree
 import logging
 

7090d66bc21bea63d922a79a09e7347abddeabdc
Giannis Melidis <giannismelidis@olery.com>
2015-05-21 16:28:26 +0200

    Removed methods that are no longer necessary in opinion detector basic class.

--- lib/opener/opinion_detector_basic.rb
+++ lib/opener/opinion_detector_basic.rb
@@ -32,77 +32,19 @@ module Opener
       @args    = options.delete(:args) || []
       @options = options
     end
-
+    
     ##
-    # Builds the command used to execute the kernel.
-    #
-    # @param [Array] args Commandline arguments passed to the command.
-    #
-    def command
-      return "#{adjust_python_path} python -E #{kernel} #{args.join(' ')}"
-    end
-
-    ##
-    # Processes an input KAF document and returns the results as a new KAF
-    # document.
+    # Processes the input KAF document.
     #
     # @param [String] input
     # @return [String]
     #
     def run(input)
-      stdout, stderr, process = capture(input)
+      options[:timestamp] = !options.delete(:no_time)
 
-      raise stderr unless process.success?
-
-      return stdout
-    end
-
-    protected
-
-    ##
-    # @return [String]
-    #
-    def adjust_python_path
-      site_packages =  File.join(core_dir, 'site-packages')
-
-      return "env PYTHONPATH=#{site_packages}:$PYTHONPATH"
-    end
-
-    ##
-    # capture3 method doesn't work properly with Jruby, so
-    # this is a workaround
-    #
-    def capture(input)
-      Open3.popen3(*command.split(" ")) {|i, o, e, t|
-        out_reader = Thread.new { o.read }
-        err_reader = Thread.new { e.read }
-        i.write input
-        i.close
-        [out_reader.value, err_reader.value, t.value]
-      }
-    end
-
-    ##
-    # @return [String]
-    #
-    def core_dir
-      return File.expand_path('../../../core', __FILE__)
-    end
-
-    ##
-    # @return [String]
-    #
-    def kernel
-      return File.join(core_dir, 'opinion_detector_basic_multi.py')
-    end
-
-    ##
-    # @return the language from the KAF
-    #
-    def language(input)
-      document = Nokogiri::XML(input)
-
-      return document.at('KAF').attr('xml:lang')
+      return Processor.new(input, options).process
     end
   end # OpinionDetectorBasic
 end # Opener
+
+

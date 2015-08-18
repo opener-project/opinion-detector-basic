caffa903653f944263b1ebf1ac21bd9afbbde1c2
Yorick Peterse <yorickpeterse@olery.com>
2015-08-18 11:53:22 +0200

    Added custom NewRelic tracers for the daemon

--- exec/opinion-detector-basic.rb
+++ exec/opinion-detector-basic.rb
@@ -4,6 +4,44 @@ require 'opener/daemons'
 
 require_relative '../lib/opener/opinion_detector_basic'
 
+Oga::XML::Parser.class_eval do
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  include NewRelic::Agent::MethodTracer
+
+  add_method_tracer(:parse)
+end
+
+Oga::XPath::Parser.class_eval do
+  class << self
+    include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+    include NewRelic::Agent::MethodTracer
+
+    add_method_tracer(:parse_with_cache, 'Oga::XPath::Parser/parse_with_cache')
+  end
+end
+
+Oga::XPath::Evaluator.class_eval do
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  include NewRelic::Agent::MethodTracer
+
+  add_method_tracer(:evaluate)
+  add_method_tracer(:evaluate_ast)
+end
+
+Opener::OpinionDetectorBasic::Processor.class_eval do
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  include NewRelic::Agent::MethodTracer
+
+  add_method_tracer(:process)
+  add_method_tracer(:terms)
+  add_method_tracer(:opinions)
+  add_method_tracer(:add_opinion_element)
+  add_method_tracer(:pretty_print)
+  add_method_tracer(:set_accumulated_strength)
+  add_method_tracer(:apply_modifiers)
+  add_method_tracer(:apply_conjunctions)
+end
+
 daemon = Opener::Daemons::Daemon.new(Opener::OpinionDetectorBasic)
 
 daemon.start
